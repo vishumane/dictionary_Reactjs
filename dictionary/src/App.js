9ca7b4f95b671568ebd1b3768261ba058cf952f2
vishumane <47922687+vishumane@users.noreply.github.com>
2021-09-08 18:26:35 +0530

    simple dictionary
    dictionary component

---
+++ dictionary/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Dictionary from'./component/dictionary/Dictionary';
+import React from'react';
+function App() {
+  return (
+    <div className="App">
+      <Dictionary/>
+    </div>
+  );
+}
+
+export default App;
